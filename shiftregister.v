2296c75cb20569368443a85345d8ccdc5815af4e
nrivkin <noah.rivkin@students.olin.edu>
2018-10-10 07:00:16 -0400

    add incomplete shift register

--- shiftregister.v
+++ shiftregister.v
@@ -5,7 +5,6 @@
 //      - serial in, parallel out
 //      - parallel in, serial out
 //------------------------------------------------------------------------
-
 module shiftregister
 #(parameter width = 8)
 (
@@ -17,9 +16,23 @@ input               serialDataIn,       // Load shift reg serially
 output [width-1:0]  parallelDataOut,    // Shift reg data contents
 output              serialDataOut       // Positive edge synchronized
 );
-
+    integer index;
     reg [width-1:0]      shiftregistermem;
     always @(posedge clk) begin
         // Your Code Here
+	// PISO mode is prioritized over SIPO mode
+	if (parallelLoad == 1) begin
+	// PISO mode
+	shiftregistermem <= {parallelDataIn[width-1:0]};
+	end else begin 
+	// SIPO mode
+	// Serial in and shift
+        for (index = 0; index < width - 1; index = index + 1) begin
+	    shiftregistermem[index + 1] <= shiftregistermem[index];
+	end
+	shiftregistermem[0] <= serialDataIn;
+	end
+	parallelDataOut <= {shiftregistermem[width-1:0]};
+	serialDataOut <= shiftregistermem[width - 1];
     end
 endmodule
