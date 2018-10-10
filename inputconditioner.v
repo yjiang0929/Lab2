236b85bfbdc0ea071bfc5e477f7d94763e7c784e
yjiang0929 <yichenjiang0929@gmail.com>
2018-10-09 23:00:40 -0400

    finish input conditioner

--- inputconditioner.v
+++ inputconditioner.v
@@ -16,20 +16,24 @@ output reg  negativeedge    // 1 clk pulse at falling edge of conditioned
 
     parameter counterwidth = 3; // Counter size, in bits, >= log2(waittime)
     parameter waittime = 3;     // Debounce delay, in clock cycles
-    
+
     reg[counterwidth-1:0] counter = 0;
     reg synchronizer0 = 0;
-    reg synchronizer1 = 0;
-    
+    reg synchronizer1 = 0;  //adding additional D-flip flop to reduce possibility of metastability
+
     always @(posedge clk ) begin
+        positiveedge <= 0;
+        negativeedge <= 0;
         if(conditioned == synchronizer1)
             counter <= 0;
         else begin
             if( counter == waittime) begin
                 counter <= 0;
+                positiveedge <= (!conditioned) & synchronizer1;
+                negativeedge <= (conditioned) & (!synchronizer1);
                 conditioned <= synchronizer1;
             end
-            else 
+            else
                 counter <= counter+1;
         end
         synchronizer0 <= noisysignal;
