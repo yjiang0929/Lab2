ec643a51b6e3b63f4db0d2a6e57cbee45c1f8c7d
nrivkin <noah.rivkin@students.olin.edu>
2018-10-10 10:17:45 -0400

    implement untested shiftregister.v

--- shiftregister.v
+++ shiftregister.v
@@ -13,8 +13,8 @@ input               peripheralClkEdge,  // Edge indicator
 input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
 input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
 input               serialDataIn,       // Load shift reg serially
-output [width-1:0]  parallelDataOut,    // Shift reg data contents
-output              serialDataOut       // Positive edge synchronized
+output reg [width-1:0]  parallelDataOut,    // Shift reg data contents
+output reg             serialDataOut       // Positive edge synchronized
 );
     integer index;
     reg [width-1:0]      shiftregistermem;
@@ -26,14 +26,14 @@ output              serialDataOut       // Positive edge synchronized
 	    shiftregistermem <= {parallelDataIn[width-1:0]};
 	end 
 	else if (peripheralClkEdge == 1) begin 
-	// SIPO mode
-	// Serial in and shift
+	    // SIPO mode
+	    // Serial in and shift
             for (index = 0; index < width - 1; index = index + 1) begin
 	        shiftregistermem[index + 1] <= shiftregistermem[index];
 	    end
 	    shiftregistermem[0] <= serialDataIn;
 	end
-	assign parallelDataOut = {shiftregistermem[width-1:0]};
-	assign serialDataOut = shiftregistermem[width - 1];
+	parallelDataOut <= {shiftregistermem[width-1:0]};
+	serialDataOut <= shiftregistermem[width - 1];
     end
 endmodule
