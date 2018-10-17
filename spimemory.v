3edb3ceb4169368605051160cdc169f8aa0e884b
yjiang0929 <yichenjiang0929@gmail.com>
2018-10-17 19:28:52 -0400

    change stuff

--- spimemory.v
+++ spimemory.v
@@ -1,6 +1,5 @@
-//------------------------------------------------------------------------
-// SPI Memory
-//------------------------------------------------------------------------
+`timescale 1 ns / 1 ps
+
 `include "addrlatch.v"
 `include "datamemory.v"
 `include "dff.v"
@@ -8,26 +7,30 @@
 `include "shiftregister.v"
 `include "fsm.v"
 
-module spiMemo
+//------------------------------------------------------------------------
+// SPI Memory
+//------------------------------------------------------------------------
+
+module spiMemory
 (
-    input        clk,
-    input  SCLK, // sw0 = MOSI; sw1 = SCLK;
-    input  CS,     // btn = ;
-    output miso_pin,
-    input mosi_pin,
-    output [7:0] sRegOutP
+    input           clk,        // FPGA clock
+    input           sclk_pin,   // SPI clock
+    input           cs_pin,     // SPI chip select
+    output          miso_pin,   // SPI master in slave out
+    input           mosi_pin,   // SPI master out slave in
+    output [3:0]    leds        // LEDs for debugging
 )
     wire condCS, condMOSI, condSCLK, posCS, posMOSI, pcoss1, negCS, negMOSI, negSCLK;
     wire serialout;
     wire MISO_BUFF, DM_WE, ADDR_WE, SR_WE;
-    wire[7:0] dataOut, addrOut;
+    wire[7:0] sRegOutP, dataOut, addrOut;
     wire[6:0] address;
     wire Q;
 
     // Memory for stored operands (parametric width set to 4 bits)
-    inputconditioner butt0cond(clk, CS, condCS, posCS, negCS);
+    inputconditioner butt0cond(clk, cs_pin, condCS, posCS, negCS);
     inputconditioner MOSIcond(clk, MOSI, condMOSI, posMOSI, negMOSI);
-    inputconditioner SCLKcond(clk, SCLK, condSCLK, posSCLK, negSCLK);
+    inputconditioner SCLKcond(clk, sclk_pin, condSCLK, posSCLK, negSCLK);
 
     fsm fsm0(posSCLK, condCS, sRegOutP[0], MISO_BUFF, DM_WE, ADDR_WE, SR_WE);
 
@@ -42,18 +45,5 @@ module spiMemo
     dff dff0(serialout, negSCLK, clk, Q);
 
     assign miso_pin = MISO_BUFF? serialout : 'bz; //Buffer
-
-endmodule
-
-module spiMemory
-(
-    input           clk,        // FPGA clock
-    input           sclk_pin,   // SPI clock
-    input           cs_pin,     // SPI chip select
-    output          miso_pin,   // SPI master in slave out
-    input           mosi_pin,   // SPI master out slave in
-    output [3:0]    leds        // LEDs for debugging
-)
-
 
 endmodule
